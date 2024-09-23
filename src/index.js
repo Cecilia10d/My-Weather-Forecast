9a7b67e29b1fdc4b29013191b3315e4614e6eea3
Cecilia <ncecilia32d@gmail.com>
2024-09-23 14:14:24 +0300

    Added search functionality using API.

--- src/index.js
+++ src/index.js
@@ -1,9 +1,27 @@
+function refreshWeather(response) {
+  let temparatureElement = document.querySelector("#temparature");
+  let temparature= response.data.temparature.current;
+   let cityElement =document.querySelector ("#city");
+
+   cityElement.innerHHTML=response.data.city;
+   temparatureElement.innerHTML=Math.round(temparature);
+
+}
+
+function searchCity(city) {
+  let apiKey="53242481c00572faoba42c192d37atb8";
+  let apiUrl='https://api.shecodes.io/weather/v1/current?query=${city}&key={apiKey}';
+  Axio.get(apiUrl).then(refreshWeather);
+}
+
 function handleSearchSubmit (event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
-  let cityElement = document.querySelector("#city");
-  cityElement.innerHTML = searchInput.value;
+
+searchCity(searchInput.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
+
+searchCity("Nairobi");
