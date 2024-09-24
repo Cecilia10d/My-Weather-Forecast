1b0a7830efed7c299ff8b5f59ec3ce72d29dd167
Cecilia <ncecilia32d@gmail.com>
2024-09-24 11:52:46 +0300

    updated html and javascript.

--- src/index.js
+++ src/index.js
@@ -3,8 +3,8 @@ function refreshWeather(response) {
   let temperature = response.data.temperature.current;
   let cityElement = document.querySelector("#city");
   let descriptionElement = document.querySelector("#description");
-  let humidityElement =document.querySelector("humidity");
-  let windSpeedElement =document.querySelector("wind-speed");
+  let humidityElement =document.querySelector("#humidity");
+  let windSpeedElement =document.querySelector("#wind-speed");
     let timeElement = document.querySelector("#time");
    let date = new Date(response.data.time * 1000);
      let iconElement = document.querySelector("#icon");
